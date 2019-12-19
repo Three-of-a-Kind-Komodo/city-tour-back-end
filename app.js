109c8be32a2f4fbd6fea90d9ef7846f6d06e44cb
fariisiibrahiim <farisibrahmi@gmail.com>
2019-12-19 15:36:09 +0700

    added nodeemailer

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var bodyParser = require("body-parser");
 var cors = require("cors");
+const nodemailer = require("nodemailer");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/user");
@@ -21,5 +22,52 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/contents", contentsRouter);
+
+app.post("/contacts", async (req, res) => {
+  try {
+    const { name, email, message } = req.body;
+
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      secure: false,
+      auth: {
+        user: "3ofakindkomodo@gmail.com",
+        pass: "3ofakind"
+      }
+    });
+
+    let mailoptions = {
+      from: "3ofakindkomodo@gmail.com",
+      to: email,
+      subject: "Thank you for contacting us",
+      text: message
+    };
+
+    // let message = {
+    //   from: " 'Admin 👻' <3ofakindkomodo@gmail.com>",
+    //   to: "farisibrahmi@gmail.com",
+    //   subject: "Order Confirmation",
+    //   text: "Thank you, the booking has been booked, please pay later",
+    //   html: "<h1>Thank you, the booking has been booked, please pay later</h1>"
+    // };
+
+    await transporter.sendMail(mailoptions, err => {
+      if (!err) {
+        res.send({
+          message: "Email send"
+        });
+      } else {
+        res.status(400).send({
+          message: "send email fail"
+        });
+      }
+    });
+  } catch (error) {
+    res.status(400).send({
+      message: "something error when send email",
+      error: error.message
+    });
+  }
+});
 
 module.exports = app;
